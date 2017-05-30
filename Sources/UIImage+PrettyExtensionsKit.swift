d45ce3cc9be411c768b8cdda196b0a66a99a590c
Oleksii Naboichenko <naboichenko@zfort.com>
2017-05-30 16:42:11 +0300

    Added method for fill image with color

--- Sources/UIImage+PrettyExtensionsKit.swift
+++ Sources/UIImage+PrettyExtensionsKit.swift
@@ -107,3 +107,34 @@ public extension UIImage {
         return newImage
     }
 }
+
+public extension UIImage {
+
+    public func prettyMaskWithColor(color: UIColor) -> UIImage? {
+        guard let maskImage = cgImage else {
+            return nil
+        }
+        
+        let width = maskImage.width
+        let height = maskImage.height
+        let bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        
+        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue) else {
+            return nil
+        }
+        
+        context.clip(to: bounds, mask: maskImage)
+        context.setFillColor(color.cgColor)
+        context.fill(bounds)
+        
+        guard let cgImage = context.makeImage() else {
+            return nil
+        }
+        
+        return UIImage(cgImage: cgImage)
+    }
+
+}
